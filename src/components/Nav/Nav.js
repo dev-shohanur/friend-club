995e84c7d4318f07583745d88005903c31e0b6f0
dvsrshohan <shohan.ridigames@gmail.com>
2022-09-30 19:44:24 +0600

    Add React Toast

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,7 +2,11 @@ import React, { useState } from 'react';
 import Break_btn from '../Break_btn/Break_btn';
 import Exercise from '../Exercise/Exercise';
 import UserInfo from '../UserInfo/UserInfo';
-import './Nav.css'
+import './Nav.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
 
 const Nav = (props) => {
     const [breakTime, setBreakTime] = useState([]);
@@ -10,6 +14,7 @@ const Nav = (props) => {
         localStorage.setItem('breakTime', props);
         setBreakTime(JSON.parse(localStorage.getItem('breakTime')));
     };
+    const notify = () => toast("Wow Activity Completed!");
     return (
         <div className='nav-bar'>
             <UserInfo></UserInfo>
@@ -20,7 +25,10 @@ const Nav = (props) => {
                 totalTime={props.totalTime}
                 breakTime={breakTime}
             ></Exercise>
-            <button className='btn btn-primary w-100'>Activity Completed</button>
+            
+            <button onClick={notify} className='btn btn-primary w-100'>Activity Completed</button>
+            <ToastContainer />
+            
         </div>
     );
 };
